dde7baa2db13a9c5a3670f8caf83aff752ec474c
Bruma Sebastian <sebastianbruma24@gmail.com>
2021-04-26 13:28:40 +0300

    Identificarea claselor necesare.

---
+++ Car.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+/*
+ La un târg de mașini trebuie înregistrate toate mașinile care au fost vândute (cumpărate).
+Pentru fiecare mașină se vor indica: nume vânzător, nume cumpărător, tip mașină (nume
+firmă + model, exp.: Firma: Opel, Model: Astra 1.4), an fabricație, culoare, optiuni, data
+tranzacție, preț. Pe lângă operațiile de introducere, editare și ștergere, aplicația trebuie să
+afișeze următoarele rapoarte:
+• cea mai căutată mașină ca și firmă sau ca model, într-o anumită perioadă;
+• un grafic al prețului pentru un anumit model, pe o anumită perioadă de timp;
+• afișarea tranzacțiilor dintr-o anumită zi.
+Observație: la introducerea unei noi tranzacții, se va avertiza printr-un mesaj dacă există o
+persoană care cumpără mai multe mașini în aceeași zi sau dacă o persoană vinde mai multe
+mașini în aceeași zi.
+ */
+namespace TemaProiectPIU
+{
+    class Car
+    {
+        public string NumeVanzator { get; set; }
+        public string NumeCumparator { get; set; }
+        public string Firma { get; set; }
+        public string Model { get; set; }
+        public int AnFabricatie { get; set; }
+        public string Culoare { get; set; }
+        public List<string> Optiuni { get; set; }
+        public DateTime DataTranzactie { get; set; }
+        public float Pret { get; set; }
+    }
+}
